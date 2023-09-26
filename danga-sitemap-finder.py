e1302a1cc1426f8a3c137dd736b2993b16fe7543
hablasharif <138958393+hablasharif@users.noreply.github.com>
2023-09-26 14:11:38 +0600

    Update danga-sitemap-finder.py

--- danga-sitemap-finder.py
+++ danga-sitemap-finder.py
@@ -3,10 +3,13 @@ import requests
 from bs4 import BeautifulSoup
 from urllib.parse import urlparse, urljoin
 import pyperclip
+import csv
+import datetime
 
 # Define a user agent to simulate a web browser
 user_agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3"
 
+@st.cache_data
 def extract_sitemap_url(domain):
     sitemap_urls = [
         urljoin(domain, "sitemap.xml"),
@@ -28,22 +31,8 @@ def extract_sitemap_url(domain):
 
     return None  # Return None if none of the sitemap URLs were found
 
-def extract_robots_txt(domain):
-    robots_txt_url = urljoin(domain, "robots.txt")
-    try:
-        # Send an HTTP GET request with the user agent
-        headers = {"User-Agent": user_agent}
-        response = requests.get(robots_txt_url, headers=headers)
-
-        # Check if the request was successful (status code 200)
-        if response.status_code == 200:
-            return response.text
-    except requests.exceptions.RequestException as e:
-        pass
-
-    return None  # Return None if robots.txt was not found
-
-def extract_all_urls_from_sitemap(sitemap_url, progress_callback=None):
+@st.cache_data
+def extract_all_urls_from_sitemap(sitemap_url):
     url_list = []
 
     def extract_recursive(sitemap_url):
@@ -70,14 +59,54 @@ def extract_all_urls_from_sitemap(sitemap_url, progress_callback=None):
                     sub_sitemap_url = sitemapindex_element.find("loc").text
                     extract_recursive(sub_sitemap_url)
 
-                # Update progress
-                if progress_callback:
-                    progress_callback(len(url_list))
         except requests.exceptions.RequestException as e:
             pass
 
     extract_recursive(sitemap_url)
     return url_list
+
+def filter_urls(url_list):
+    filtered_urls = []
+    removed_urls = []
+
+    # Patterns to filter out
+    filter_patterns = [
+        "/casts/",
+        "/cast/",
+        "/directors/",
+        "/director/",
+        "/artist/",
+        "/artists/",
+        "/actors/",
+        "/actor/",
+        "/tag/",
+        "/tags/",
+        "/country/",
+        "/genre/",
+        "/stars/",
+        "/release-year/",
+        "/quality/",
+        "/episode-date/",
+        "/category/",
+        "/lang/",
+    ]
+
+    # Extensions to filter out
+    filter_extensions = [".jpg", ".png", ".webp", ".xml"]
+
+    for url in url_list:
+        # Check if the URL contains any filter patterns
+        if any(pattern in url for pattern in filter_patterns):
+            removed_urls.append(url)
+        else:
+            # Check if the URL has a valid extension
+            parsed_url = urlparse(url)
+            url_path = parsed_url.path
+            file_extension = url_path.split(".")[-1].lower()
+            if file_extension not in filter_extensions:
+                filtered_urls.append(url)
+
+    return filtered_urls, removed_urls
 
 def main():
     st.title("Sitemap URL Extractor")
@@ -85,10 +114,10 @@ def main():
     domain_input = st.text_area("Enter multiple domains (one per line):")
     domains = [domain.strip() for domain in domain_input.split("\n") if domain.strip()]  # Split input by lines
 
+    all_url_list = []  # Initialize the list to store URLs
+
     if st.button("Extract URLs"):
         if domains:
-            all_url_list = []  # Store URLs from all domains
-
             for domain in domains:
                 # Ensure the domain is properly formatted
                 if not domain.startswith("http://") and not domain.startswith("https://"):
@@ -100,49 +129,68 @@ def main():
                     url_list = extract_all_urls_from_sitemap(sitemap_url)
                     total_urls = len(url_list)  # Count total URLs
 
-                    progress_text = st.empty()  # Create an empty element for progress text
-
-                    def update_progress(progress):
-                        percentage = (progress / total_urls) * 100
-                        progress_text.text(f"{progress} out of {total_urls} URLs ({percentage:.2f}% completed)...")
-
-                    with st.spinner(f"Extracting URLs from {domain}..."):
-                        url_list = extract_all_urls_from_sitemap(sitemap_url, progress_callback=update_progress)
-
                     if url_list:
                         st.success(f"Found {total_urls} URLs in the sitemap of {domain}:")
                         st.text_area(f"URLs from {domain}", "\n".join(url_list))  # Display URLs in a text area
                         all_url_list.extend(url_list)
                     else:
-                        st.error(f"Failed to retrieve or extract URLs from {domain}'s sitemap.")
-                        progress_text.empty()  # Clear the progress text if extraction fails
-                else:
-                    st.error(f"No sitemap found for {domain}.")
-                
-                # Check for robots.txt
-                robots_txt_content = extract_robots_txt(domain)
-                if robots_txt_content:
-                    st.success(f"robots.txt for {domain}:")
-                    st.text_area(f"robots.txt from {domain}", robots_txt_content)  # Display robots.txt
-                else:
-                    st.warning(f"No robots.txt found for {domain}.")
+                        st.error(f"Failed to retrieve or extract URLs from {domain}.")
 
-            if all_url_list:
-                # Add a "Copy All URLs" button
-                if st.button("Copy All URLs"):
-                    # Join all URLs into a single string with line breaks
-                    all_urls_text = "\n".join(all_url_list)
-                    # Copy all the URLs to the clipboard
-                    pyperclip.copy(all_urls_text)
-                    st.success("All URLs copied to clipboard.")
+    # Button to copy all URLs to clipboard
+    if st.button("Copy All URLs"):
+        if all_url_list:
+            # Join all URLs into a single string with line breaks
+            all_urls_text = "\n".join(all_url_list)
+            # Copy all the URLs to the clipboard
+            pyperclip.copy(all_urls_text)
+            st.success("All URLs copied to clipboard.")
+
+    # Button to download URLs as a CSV file
+    if domains:
+        timestamp = datetime.datetime.now().strftime("%Y-%m-%d %A %I-%M-%p")
+        # Create a formatted file name with inputted domains and timestamp
+        formatted_domains = " ".join(domain.replace("https://", "").replace("http://", "") for domain in domains)
+        unfiltered_filename = f"Unfiltered URLs {formatted_domains} {timestamp}.csv"
+
+        # Create a download button for unfiltered URLs
+        download_button_unfiltered = st.download_button(
+            label="Download Unfiltered URLs as CSV",
+            data="\n".join(all_url_list),
+            key="download_button_unfiltered",
+            file_name=unfiltered_filename,
+        )
+
+        # Filter the URLs
+        filtered_urls, removed_urls = filter_urls(all_url_list)
+
+        # Create a formatted file name for removed URLs
+        removed_filename = f"Removed URLs {formatted_domains} {timestamp}.csv"
+
+        # Create a download button for removed URLs
+        download_button_removed = st.download_button(
+            label="Download Removed URLs as CSV",
+            data="\n".join(removed_urls),
+            key="download_button_removed",
+            file_name=removed_filename,
+        )
+
+        # Create a formatted file name for filtered URLs
+        filtered_filename = f"Filtered URLs {formatted_domains} {len(filtered_urls)} {timestamp}.csv"
+
+        # Create a download button for filtered URLs
+        download_button_filtered = st.download_button(
+            label=f"Download Filtered URLs as CSV ({len(filtered_urls)} URLs)",
+            data="\n".join(filtered_urls),
+            key="download_button_filtered",
+            file_name=filtered_filename,
+        )
 
 if __name__ == "__main__":
     main()
 
-
 # Create a link to the external URL
 url = "https://website-titles-and-h1-tag-checke.streamlit.app/"
-link_text = "VISIT THIS IF YOU WANT TO PULL WEBSITE  ALL  TITLES AND H1 TAG TITLE THEN VISIT THIS"
+link_text = "VISIT THIS IF YOU WANT TO PULL WEBSITE ALL TITLES AND H1 TAG TITLE THEN VISIT THIS"
 
 # Display the link
 st.markdown(f"[{link_text}]({url})")
